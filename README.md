a4c4dd35fc9499fe4b6987e0fa5656c146958b4a
Junsu <murane@naver.com>
2021-04-28 14:22:11 +0900

    HelloControllerTest 작성

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 1장  
 
-buildscrpit란?  
-ctrl + space로 추론  
-jcenter, mavenCentral 차이  
+* buildscrpit란?  
+* ctrl + space로 추론  
+* jcenter, mavenCentral 차이  
 
 # 2장
 
@@ -19,3 +19,15 @@ public class Application {
 자동으로 설정을 해주고 컴포넌트 스캔의 기준점이 된다.
 
 SpringApplcation.run으로 내장 톰캣을 실행한다.
+
+### /hello 컨트롤러
+
+* @RestController -> view로의 연결을 Rest로 바꿔서 json을 반환하게 한다.
+* @GetMapping -> **Mapping을 통해 특정 http메소드를 라우팅한다.
+
+
+* @RunWith(SpringRunner.class) -> Junit4일경우 필요하다  
+* @WebMvcTest -> @SpringBootTest는 빈을 모두 띄우기 때문에 
+  mvc와 관련된 controller,controllerAdvice,filter,interceptor
+  ,webMvcConfigurer등의 빈을 띄워주는 WebMvcTest를 슬라이스 테스트에 사용한다.  
+* MockMvc -> perform으로 요청을 보내고 Expect로 응답과 body를 검증한다.
---
+++ src/test/java/com/jun/study/spring/web/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.jun.study.spring.web;
+
+import com.jun.study.springboot.Application;
+import com.jun.study.springboot.web.HelloController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ContextConfiguration(classes = {Application.class})
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    void hello_리턴() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
