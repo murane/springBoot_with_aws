3bb87a4f1a3f7bd07e9a190923313f19d25a49bd
Junsu <murane@naver.com>
2021-05-09 22:11:31 +0900

    Oauth2 구글 로그인 IndexController, index.mustache 작성

--- src/main/java/com/jun/study/springboot_aws/web/IndexController.java
+++ src/main/java/com/jun/study/springboot_aws/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jun.study.springboot_aws.web;
 
+import com.jun.study.springboot_aws.config.auth.LoginUser;
+import com.jun.study.springboot_aws.config.auth.dto.SessionUser;
 import com.jun.study.springboot_aws.service.posts.PostsService;
 import com.jun.study.springboot_aws.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -12,19 +14,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
     private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts",postsService.findAllDesc());
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
